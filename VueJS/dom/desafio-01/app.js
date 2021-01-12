b73985c3388aa5893049514fcbe2ff641384c8fe
FelipePizzio <33675495+FelipePizzio@users.noreply.github.com>
2021-01-12 19:50:14 -0300

    Add files via upload

---
+++ VueJS/dom/desafio-01/app.js
@@ -0,0 +1,8 @@
+new Vue({
+    el: '#desafio',
+    data: {
+       nome: 'Felipe Pizzio',
+       idade: 28,
+       imagem: 'gato.jpg'
+    }
+})
---
+++ VueJS/dom/desafio-02/app.js
@@ -0,0 +1,11 @@
+new Vue({
+    el: '#desafio',
+    data: {
+        valor: ''
+    },
+    methods: {
+        chamarAlerta() {
+            alert('Um alerta. Fique esperto.')
+        },
+    }
+})
